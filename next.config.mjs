6a05068e01c3ec67a2caeefbad2d9ad2728fb2d6
sparrowthedev <sparrowthedev@gmail.com>
2024-06-09 21:31:31 +0100

    made minor majors

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/job-listing-',  
-    assetPrefix: '/job-listing-/',
-
-};
+const nextConfig = {};
 
 export default nextConfig;
